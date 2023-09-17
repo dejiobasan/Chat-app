66676ef2ebf67f31a2b7d2e15025a9782ca5e1f2
dejiobasan <dejiobasan@yahoo.com>
2023-09-17 01:14:31 +0100

    initial code

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": process.env.privateKey } }
+    );
+    return res.status(r.status).json(r.data);
+
+  } catch (e) {
+    return (e);
+  }
+});
+
+app.listen(3001);
